2aea798e3e3044eb4c7a57d313332c7b1f6963ea
Ashleigh Morris <ash.morris255@gmail.com>
2022-02-24 12:08:17 -0500

    lap counter, console log "win" if 3 laps completed

--- Assets/Scripts/CrossFInishLine.cs
+++ Assets/Scripts/CrossFInishLine.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrossFInishLine : MonoBehaviour
+public class CrossFinishLine : MonoBehaviour
 {
+
+    private int lapCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         //Debug.Log("CrossFinishLine has run");
     }
-    
+
+    private bool istriggered = false; 
+ 
     private void OnTriggerEnter(Collider collider){
-        if(collider.gameObject.tag == "Player"){
-            Debug.Log("u win~" + collider.gameObject.tag);
-        }
-        
+        if  (istriggered == false) {
+            if(collider.gameObject.tag == "Player"){
+                lapCount++;
+                Debug.Log(lapCount);
+                if(lapCount == 3){
+                    Debug.Log("u win~" + collider.gameObject.tag);
+                }
+            }
+            // do your things here that has to happen once
+            istriggered = true;
+        }        
     }
+    
+    void OnTriggerExit( Collider coll)
+    {
+        istriggered  = false;
+    }
+
 }
