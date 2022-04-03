46cd3905306001baec5e5f0c327ac27296ed698f
Ashleigh Morris <ash.morris255@gmail.com>
2022-04-03 13:44:17 -0400

    start of AI path finding

---
+++ Assets/Scripts/WaypointData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointData : MonoBehaviour
+{
+    public WaypointData next;
+    public void OnDrawGizmos(){
+        if(next){
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, next.transform.position); //draw red line to next waypoint (in editor)
+        }
+    }
+}
